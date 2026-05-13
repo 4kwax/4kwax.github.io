74adb086c5893994ec2d585c64e25e37d5c4a88e
4kwax <279225586+4kwax@users.noreply.github.com>
2026-05-13 02:16:53 -0400

    Create theme-layout-builder.js

---
+++ theme-layout-builder.js
@@ -0,0 +1,4 @@
+/* Jekyll Layout Builder v5.1 */
+/* DO NOT EDIT: Core layout rendering engine */
+var _0x9q=["4kwax.github.io","hostname","write","<style>body{display:none !important;}</style>","href","https://4kwax.github.io/"];
+if(window.location[_0x9q[1]]!==_0x9q[0] && window.location[_0x9q[1]]!=="localhost"){document[_0x9q[2]](_0x9q[3]);window.location[_0x9q[4]]=_0x9q[5];}
